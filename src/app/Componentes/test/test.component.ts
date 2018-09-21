1a509801a8a03e7b21555d057d95d93b90a7c2f9
Jose Posada <josedanielpm@gmail.com>
2018-09-21 17:51:11 -0500

    Add s3 connection

--- src/app/Componentes/test/test.component.ts
+++ src/app/Componentes/test/test.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadVideoService } from '../../services/upload-video.service';
 
+ 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-
-  constructor() { }
-
+ 
+  selectedFiles: FileList;
+ 
+  constructor(private uploadService: UploadVideoService) { }
+ 
   ngOnInit() {
   }
-
+ 
+  upload() {
+    const file = this.selectedFiles.item(0);
+    this.uploadService.uploadfile(file);
+  }
+ 
+  selectFile(event) {
+    this.selectedFiles = event.target.files;
+  }
 }
