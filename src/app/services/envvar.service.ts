1b62e13b931747dde19f2176eaae091e1cdde28b
scadavidg <31990704+scadavidg@users.noreply.github.com>
2018-09-25 22:37:43 -0500

    ENV

--- src/app/services/envvar.service.ts
+++ src/app/services/envvar.service.ts
@@ -9,7 +9,7 @@ export class EnvvarService {
   env:runtimeEnv = runtimeEnv();
   constructor() {  }
   public getApi(){
-    console.log(this.env.REACT_APP_URL); 
+    console.log(JSON.stringify(this.env.REACT_APP_URL)); 
     
       
   }
